a694c194ee26addfb1c97bb1079f9afe4e9fab51
harrisonhall <harrisonchristianhall@gmail.com>
2022-04-13 16:29:22 -0400

    Updated sgs godot client with convenient features

--- clients/godot/sgs_client_godot.gd
+++ clients/godot/sgs_client_godot.gd
@@ -1,15 +1,18 @@
 ## SGS Godot client
+### Note: data_received signal is expected to be connected with a function called data_received
 
 extends Node
+class_name SGSNetwork
 
-
-var client = WebSocketClient.new()
+var client : WebSocketClient = WebSocketClient.new()
 
 signal data_received
+signal server_connected
+signal server_disconnected
 signal lobby_connected
 signal lobby_disconnected
 
-var current_url = ""
+var current_url : String = ""
 
 
 func _ready():
@@ -21,6 +24,7 @@ func _ready():
 func _connection_established(proto = ""):
 	print("SGS: Connected to server: " + current_url)
 	connected_to_sgs = true
+	emit_signal("server_connected")
 
 func _data_received():
 	var raw = client.get_peer(1).get_packet().get_string_from_utf8()
@@ -37,8 +41,10 @@ func _data_received():
 			connecting_to_lobby = false
 			connected_to_lobby = true
 			current_lobby = obj.get("lobby")  # Should always be in packet
+			current_id = obj.get("data", {}).get("player_id", -1)
 			print("Debug: Set lobby to ", current_lobby)
 			emit_signal("lobby_connected")
+			
 		
 		if "is_leader" in obj.get("data"):
 			lobby_leader = obj.get("data", {}).get("is_leader", false)
@@ -48,21 +54,36 @@ func _data_received():
 		prints("SGS: Error message:", obj)
 	
 	if obj.get("type", "error") == "initialization_data":
-		emit_signal("data_received", obj["data"])
+		_smart_emit_data_received(obj.get("data", {}))
 	
 	if obj.get("type", "error") == "data":
-		emit_signal("data_received", obj["data"])
+		_smart_emit_data_received(obj.get("data", {}))
+
+var _queued_data : Array = []
+func _smart_emit_data_received(data : Dictionary):
+	# Check if current scene has connected signal to our data_received, if not,
+	# queue it for next time
+	var current_scene : Node = get_tree().get_current_scene()
+	if self.is_connected("data_received", current_scene, "data_received"):
+		for prev_data in _queued_data:
+			emit_signal("data_received", prev_data)
+		_queued_data.clear()
+		emit_signal("data_received", data)
+	else:
+		_queued_data.append(data)
 
 func _connection_closed(was_clean = false):
 	prints("Closed:", was_clean)
 	set_process(false)
 	_reset_state_variables()
+	emit_signal("server_disconnected")
 	emit_signal("lobby_disconnected")
 
 func _connection_error(was_clean = false):
 	prints("Error:", was_clean)
 	set_process(false)
 	_reset_state_variables()
+	emit_signal("server_disconnected")
 	emit_signal("lobby_disconnected")
 
 func _process(_delta):
@@ -75,6 +96,7 @@ func _reset_state_variables():
 	lobby_leader = false
 	current_lobby = ""
 	current_game = ""
+	current_id = -1
 
 # State variables
 var connected_to_sgs = false
@@ -83,6 +105,11 @@ var connected_to_lobby = false
 var lobby_leader = false
 var current_lobby = ""
 var current_game = ""
+var current_id = -1
+
+func disconnect_from_server():
+	if connected_to_server():
+		client.disconnect_from_host()
 
 func connect_to_server(address):
 	var success = client.connect_to_url(address)
@@ -128,7 +155,6 @@ func send_data(obj):
 		"game": current_game
 	}
 	var jstr = JSON.print(message)
-	prints("Debug: Sending:", message, jstr)
 	client.get_peer(1).put_packet(jstr.to_utf8())
 	return true
 
@@ -142,9 +168,11 @@ func send_initialization(obj):
 		"game": current_game
 	}
 	var jstr = JSON.print(message)
-	prints("Debug: Sending init data:", message, jstr)
 	client.get_peer(1).put_packet(jstr.to_utf8())
 	return true
 
 func is_leader():
 	return (lobby_leader and connected_to_lobby)
+
+func player_id():
+	return current_id
